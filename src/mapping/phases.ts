6bd479ccc5d1b39e2aa6d748b341e0100d359211
Sebastián Galiano <sgaliano@gmail.com>
2019-09-27 01:05:47 -0300

    Add current phase

--- src/mapping/phases.ts
+++ src/mapping/phases.ts
@@ -1,5 +1,23 @@
 import { NewPhase } from '../../generated/Kleros/KlerosLiquid'
 
+import { getSummaryEntity } from './core'
+
 export function handleNewPhase(event: NewPhase): void {
-  // TODO
+  let summary = getSummaryEntity()
+  summary.currentPhase = toPhase(event.params._phase)
+  summary.lastPhaseChange = event.block.timestamp
+
+  summary.save()
+}
+
+function toPhase(phase: i32): string {
+  switch (phase) {
+    default:
+    case 0:
+      return 'STAKING'
+    case 1:
+      return 'GENERATING'
+    case 2:
+      return 'DRAWING'
+  }
 }
