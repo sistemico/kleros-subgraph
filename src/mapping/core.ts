21e2e4d1fcc21973da018dfdbf233b4d425d6126
Sebastián Galiano <sgaliano@gmail.com>
2019-09-26 22:31:22 -0300

    Rename summary entity

--- src/mapping/core.ts
+++ src/mapping/core.ts
@@ -2,7 +2,7 @@ import { Address } from '@graphprotocol/graph-ts'
 
 import { KlerosLiquid } from '../../generated/Kleros/KlerosLiquid'
 
-import { Kleros } from '../../generated/schema'
+import { KlerosSummary } from '../../generated/schema'
 
 import { ZERO } from './utils'
 
@@ -10,20 +10,20 @@ import { ZERO } from './utils'
 const KLEROS_LIQUID_ADDRESS = '0x988b3a538b618c7a603e1c11ab82cd16dbe28069'
 
 // Summary entities
-const KLEROS_SUMMARY_ID = '1'
+const KLEROS_SUMMARY_ID = '0'
 
 export function getKlerosContract(): KlerosLiquid {
   return KlerosLiquid.bind(Address.fromString(KLEROS_LIQUID_ADDRESS))
 }
 
-export function getSummaryEntity(): Kleros {
-  let summary = Kleros.load(KLEROS_SUMMARY_ID)
+export function getSummaryEntity(): KlerosSummary {
+  let summary = KlerosSummary.load(KLEROS_SUMMARY_ID)
 
   if (summary == null) {
-    summary = new Kleros(KLEROS_SUMMARY_ID)
+    summary = new KlerosSummary(KLEROS_SUMMARY_ID)
     summary.courtCount = ZERO
     summary.disputeCount = ZERO
   }
 
-  return summary as Kleros
+  return summary as KlerosSummary
 }
