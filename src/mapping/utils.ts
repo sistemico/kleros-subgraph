2856c2313b5efd7c42270ae4f8c2619bf64b89c0
Sebastián Galiano <sgaliano@gmail.com>
2019-09-28 01:01:32 -0300

    Fix wrong method

--- src/mapping/utils.ts
+++ src/mapping/utils.ts
@@ -5,5 +5,5 @@ export let ZERO = BigInt.fromI32(0)
 export let ZERO_DECIMAL = BigDecimal.fromString('0')
 
 export function uid(event: EthereumEvent): string {
-  return event.transaction.hash.toString() + '-' + event.logIndex.toString()
+  return event.transaction.hash.toHexString() + '-' + event.logIndex.toString()
 }
