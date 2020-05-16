d412059797572723bae7cde9353e47a0deef38d8
Sebastián Galiano <sgaliano@gmail.com>
2020-05-16 11:12:13 -0300

    Bump dependencies

--- src/mapping/utils.ts
+++ src/mapping/utils.ts
@@ -1,9 +1,9 @@
-import { BigDecimal, BigInt, EthereumEvent } from '@graphprotocol/graph-ts'
+import { BigDecimal, BigInt, ethereum } from '@graphprotocol/graph-ts'
 
 export let ONE = BigInt.fromI32(1)
 export let ZERO = BigInt.fromI32(0)
 export let ZERO_DECIMAL = BigDecimal.fromString('0')
 
-export function uid(event: EthereumEvent): string {
+export function uid(event: ethereum.Event): string {
   return event.transaction.hash.toHexString() + '-' + event.logIndex.toString()
 }
